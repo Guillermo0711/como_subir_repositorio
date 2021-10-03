4be8cc8ddd26cb0fd19586009364a8bd2abe41e9
Guillermo0711 <100realfrancisco@gmail.com>
2021-10-02 19:26:03 -0600

    agregar espacios

--- src/subir_repositorio/Subir_repositorio.java
+++ src/subir_repositorio/Subir_repositorio.java
@@ -11,6 +11,8 @@ public class Subir_repositorio {
         System.out.println("4) git add .");
         System.out.println("5) git commit -m \"Primer Commit\"");
         System.out.println("6) Copie los 3 comandos de Git Hub");
+        
+        
         System.out.println("\n\n\nPara subir actualizaciones");
         System.out.println("1) git add .");
         System.out.println("2) git commit -m \"Segundo commit\"");
