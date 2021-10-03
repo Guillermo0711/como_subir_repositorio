b638e8cee967f648e6cc16c69356e5a6b32f4f44
Guillermo0711 <100realfrancisco@gmail.com>
2021-10-02 19:22:20 -0600

    primer commit

---
+++ src/subir_repositorio/Subir_repositorio.java
@@ -0,0 +1,22 @@
+
+package subir_repositorio;
+
+public class Subir_repositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Primera vez...");
+        System.out.println("1) git config --global user.name \"Nombre de usuario de Github o bitbucket\" ");
+        System.out.println("2) git config --global user.email \"Correo electrónico usado en GitHub o bitbucket\" ");
+        System.out.println("3) git init ");
+        System.out.println("4) git add .");
+        System.out.println("5) git commit -m \"Primer Commit\"");
+        System.out.println("6) Copie los 3 comandos de Git Hub");
+        System.out.println("\n\n\nPara subir actualizaciones");
+        System.out.println("1) git add .");
+        System.out.println("2) git commit -m \"Segundo commit\"");
+        System.out.println("3) git pull");
+        System.out.println("4) git push");
+        
+    }
+    
+}
